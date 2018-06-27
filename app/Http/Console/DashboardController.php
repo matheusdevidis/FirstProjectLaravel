9f061a71132eb4a3523e267db674c73d036bc2ca
matheusdevidis <40633088+matheusdevidis@users.noreply.github.com>
2018-06-27 09:50:31 -0300

    Create DashboardController.php

---
+++ app/Http/Console/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
+use App\Validators\UserValidator;
+
+class DashboardController extends Controller
+{
+   private $repository;
+   private $validator;
+
+    public function __construct(UserRepository $repository, UserValidator $validator)
+    {
+        $this->repository = $repository;
+        $this->validator  = $validator;
+    }
+
+    public function auth()
+    {
+        echo 'estamos no auth (dashboard)';
+    }
+}
